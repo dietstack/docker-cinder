0032674e6c6263d60234e411592a7a6f8e6ee105
Kamil Madac <kamil.madac@gmail.com>
2017-08-07 12:22:05 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,70 @@
+FROM debian:stretch-slim
+MAINTAINER Kamil Madac (kamil.madac@gmail.com)
+
+# Apply source code patches
+RUN mkdir -p /patches
+COPY patches/* /patches/
+
+RUN echo 'APT::Install-Recommends "false";' >> /etc/apt/apt.conf && \
+    echo 'APT::Get::Install-Suggests "false";' >> /etc/apt/apt.conf && \
+    apt update; apt install -y ca-certificates wget python libpython2.7; \
+    update-ca-certificates; \
+    wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py; \
+    python get-pip.py; \
+    rm get-pip.py; \
+    wget https://raw.githubusercontent.com/openstack/requirements/stable/newton/upper-constraints.txt -P /app && \
+    /patches/stretch-crypto.sh && \
+    apt-get clean && apt autoremove && \
+    rm -rf /var/lib/apt/lists/*; rm -rf /root/.cache
+
+# Source codes to download
+ENV SVC_NAME=cinder
+ENV REPO="https://github.com/openstack/$SVC_NAME" BRANCH="stable/newton" COMMIT="1b3ab80f1c45"
+
+# Install glance with dependencies
+ENV BUILD_PACKAGES="git build-essential libssl-dev libffi-dev python-dev"
+
+# Download source codes and install glance
+RUN apt update; apt install -y $BUILD_PACKAGES && \
+    if [ -z $REPO ]; then \
+      echo "Sources fetching from releases $RELEASE_URL"; \
+      wget $RELEASE_URL && tar xvfz $SVC_VERSION.tar.gz -C / && mv $(ls -1d $SVC_NAME*) $SVC_NAME && \
+      cd /$SVC_NAME && pip install -r requirements.txt -c /app/upper-constraints.txt && PBR_VERSION=$SVC_VERSION python setup.py install; \
+    else \
+      if [ -n $COMMIT ]; then \
+        cd /; git clone $REPO --single-branch --branch $BRANCH; \
+        cd /$SVC_NAME && git checkout $COMMIT; \
+      else \
+        git clone $REPO --single-branch --depth=1 --branch $BRANCH; \
+      fi; \
+      cd /$SVC_NAME; pip install -r requirements.txt -c /app/upper-constraints.txt && python setup.py install && \
+      rm -rf /$SVC_NAME/.git; \
+    fi; \
+    pip install supervisor PyMySQL python-memcached && \
+    apt remove -y --auto-remove $BUILD_PACKAGES &&  \
+    apt-get clean && apt autoremove && \
+    rm -rf /var/lib/apt/lists/* && rm -rf /root/.cache
+
+# prepare directories for storing image files and copy configs
+RUN mkdir -p /var/lib/$SVC_NAME/images /etc/SVC_NAME /etc/supervisord /var/log/supervisord; cp -a /$SVC_NAME/etc/* /etc/$SVC_NAME
+
+# copy supervisor config
+COPY configs/supervisord/supervisord.conf /etc
+
+# copy configs
+COPY configs/$SVC_NAME/* /etc/glance/
+
+# external volume
+VOLUME /$SVC_NAME-override
+
+# copy startup scripts
+COPY scripts /app
+
+# Define workdir
+WORKDIR /app
+RUN chmod +x /app/*
+
+ENTRYPOINT ["/app/entrypoint.sh"]
+
+# Define default command.
+CMD ["/usr/local/bin/supervisord", "-c", "/etc/supervisord.conf"]
---
+++ test.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+# Integration test for glance service
+# Test runs mysql,memcached,keystone and glance container and checks whether glance is running on public and admin ports
+
+DOCKER_PROJ_NAME=${DOCKER_PROJ_NAME:-''}
+CONT_PREFIX=test
+
+. lib/functions.sh
+
+http_proxy_args="-e http_proxy=${http_proxy:-} -e https_proxy=${https_proxy:-} -e no_proxy=${no_proxy:-}"
+
+cleanup() {
+    echo "Clean up ..."
+    docker stop ${CONT_PREFIX}_mariadb
+    docker stop ${CONT_PREFIX}_memcached
+    docker stop ${CONT_PREFIX}_keystone
+    docker stop ${CONT_PREFIX}_glance
+
+    docker rm -v ${CONT_PREFIX}_mariadb
+    docker rm -v ${CONT_PREFIX}_memcached
+    docker rm -v ${CONT_PREFIX}_keystone
+    docker rm -v ${CONT_PREFIX}_glance
+}
+
+cleanup
+
+##### Start Containers
+
+echo "Starting mariadb container ..."
+docker run  --net=host -d -e MYSQL_ROOT_PASSWORD=veryS3cr3t --name ${CONT_PREFIX}_mariadb \
+       mariadb:10.1
+
+echo "Wait till mariadb is running ."
+wait_for_port 3306 120
+
+echo "Starting Memcached node (tokens caching) ..."
+docker run -d --net=host -e DEBUG= --name ${CONT_PREFIX}_memcached memcached
+
+# build glance container for current sources
+./build.sh
+
+echo "Wait till Memcached is running ."
+wait_for_port 11211 30
+
+# create databases
+create_db_osadmin keystone keystone veryS3cr3t veryS3cr3t
+create_db_osadmin glance glance veryS3cr3t veryS3cr3t
+
+echo "Starting keystone container"
+docker run -d --net=host \
+           -e DEBUG="true" \
+           -e DB_SYNC="true" \
+           $http_proxy_args \
+           --name ${CONT_PREFIX}_keystone ${DOCKER_PROJ_NAME}keystone:latest
+
+echo "Wait till keystone is running ."
+
+wait_for_port 5000 120
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "Error: Port 5000 (Keystone) not bounded!"
+    exit $ret
+fi
+
+wait_for_port 35357 120
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "Error: Port 35357 (Keystone Admin) not bounded!"
+    exit $ret
+fi
+
+echo "Starting glance container"
+GLANCE_TAG=$(docker images | grep -w glance | head -n 1 | awk '{print $2}')
+docker run -d --net=host \
+           -e DEBUG="true" \
+           -e DB_SYNC="true" \
+           -e LOAD_META="true" \
+           $http_proxy_args \
+           --name ${CONT_PREFIX}_glance ${DOCKER_PROJ_NAME}glance:$GLANCE_TAG
+
+##### TESTS #####
+
+wait_for_port 9191 120
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "Error: Port 9191 (Glance Registry) not bounded!"
+    docker logs ${CONT_PREFIX}_glance
+    exit $ret
+fi
+
+wait_for_port 9292 120
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "Error: Port 9292 (Glance API) not bounded!"
+    docker logs ${CONT_PREFIX}_glance
+    exit $ret
+fi
+
+echo "Return code $?"
+
+# bootstrap openstack settings and upload image to glance
+set +e
+docker run --net=host --rm $http_proxy_args ${DOCKER_PROJ_NAME}osadmin /bin/bash -c ". /app/tokenrc; bash /app/bootstrap.sh"
+ret=$?
+if [ $ret -ne 0 ] && [ $ret -ne 128 ]; then
+    echo "Error: Keystone bootstrap error ${ret}!"
+    exit $ret
+fi
+set -e
+
+docker run --net=host --rm $http_proxy_args ${DOCKER_PROJ_NAME}osadmin /bin/bash -c ". /app/adminrc; openstack image create --container-format bare --disk-format qcow2 --file /app/cirros.img --public cirros"
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "Error: Cirros image import error ${ret}!"
+    exit $ret
+fi
+
+echo "======== Success :) ========="
+
+if [[ "$1" != "noclean" ]]; then
+    cleanup
+fi
